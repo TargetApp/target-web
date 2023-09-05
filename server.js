c5439a41bd4b98a8eee5972a15628b170ca2cead
gr-xbY <gabriel.rampazzo@gmail.com>
2023-09-05 20:37:10 -0300

    package engines

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const app = express();
 
 const appName = 'target-app';
 const outputPath = `${__dirname}/dist/${appName}`;
+const serverPort = process.env.PORT || 8080;
 
 app.use(express.static(outputPath));
 app.get('/*', (req, res) => {
   res.sendFile(`${outputPath}/index.html`);
 });
-app.listen(process.env.PORT, () => {
-    console.log(`${appName} listening on port ${process.env.PORT}`)
+app.listen(serverPort, () => {
+    console.log(`${appName} listening on port ${serverPort}`)
 });
